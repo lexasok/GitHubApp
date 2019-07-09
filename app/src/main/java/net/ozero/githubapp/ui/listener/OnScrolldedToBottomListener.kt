16b4e5bcd47076d61663a6bfec262293e4b8dac5
Alexey <alexnapoleonit@gmail.com>
2019-07-09 13:37:57 +0300

    Bug fixed

--- app/src/main/java/net/ozero/githubapp/ui/listener/OnScrolldedToBottomListener.kt
+++ app/src/main/java/net/ozero/githubapp/ui/listener/OnScrolldedToBottomListener.kt
@@ -3,13 +3,13 @@ package net.ozero.githubapp.ui.listener
 import androidx.recyclerview.widget.RecyclerView
 
 class OnScrolldedToBottomListener(
-    private val onSrolledToBottom: () -> Unit
+    private val onScrolledToBottom: () -> Unit
 ) : RecyclerView.OnScrollListener() {
     override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
         super.onScrollStateChanged(recyclerView, newState)
         if (!recyclerView.canScrollVertically(1)
             && newState == RecyclerView.SCROLL_STATE_IDLE) {
-            onSrolledToBottom
+            onScrolledToBottom()
         }
     }
 }
